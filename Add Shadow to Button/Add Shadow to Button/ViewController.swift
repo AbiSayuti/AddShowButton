6de23110171353f01a04daa767c9e17424e59337
AbiSayuti <32661598+AbiSayuti@users.noreply.github.com>
2017-10-10 14:10:03 +0700

    Add shadow button by properties and code

---
+++ Add Shadow to Button/Add Shadow to Button/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Add Shadow to Button
+//
+//  Created by Abi Sayuti on 10/10/17.
+//  Copyright © 2017 Abi Sayuti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var ButtonShadow: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //menambahkan warna shadow
+        ButtonShadow.layer.shadowColor = UIColor.black.cgColor
+        //menambahkan ukuran offset width and height
+        ButtonShadow.layer.shadowOffset = CGSize(width: 5, height: 5)
+        //menambahkan shadow radius
+        ButtonShadow.layer.shadowRadius = 5
+        //menambahkan opasitinya jadi 1.0
+        ButtonShadow.layer.shadowOpacity = 1.0
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
